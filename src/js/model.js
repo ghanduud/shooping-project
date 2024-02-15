0744541c679ac0d039230c5025b50b5d1e1da594
Ghanduud <omar.ghandy780@gmail.com>
2024-02-15 23:53:00 +0200

    finishing design

---
+++ src/js/model.js
@@ -0,0 +1,68 @@
+const shoppingItems = [
+	{
+		id: 1,
+		itemName: 'Samsung A70',
+		cat: 'Phone',
+		price: 10000,
+		favourite: false,
+	},
+	{
+		id: 2,
+		itemName: 'OPPO A70',
+		cat: 'Phone',
+		price: 10000,
+		favourite: false,
+	},
+	{
+		id: 3,
+		itemName: 'Samsung A70',
+		cat: 'Phone',
+		price: 10000,
+		favourite: false,
+	},
+	{
+		id: 4,
+		itemName: 'Samsung A70',
+		cat: 'Phone',
+		price: 10000,
+		favourite: false,
+	},
+	{
+		id: 5,
+		itemName: 'Samsung A70',
+		cat: 'Phone',
+		price: 10000,
+		favourite: false,
+	},
+	{
+		id: 6,
+		itemName: 'Samsung A70',
+		cat: 'Phone',
+		price: 10000,
+		favourite: false,
+	},
+	{
+		id: 7,
+		itemName: 'Samsung A70',
+		cat: 'Phone',
+		price: 10000,
+		favourite: false,
+	},
+	{
+		id: 8,
+		itemName: 'Samsung A70',
+		cat: 'Phone',
+		price: 10000,
+		favourite: false,
+	},
+	{
+		id: 9,
+		itemName: 'Samsung A70',
+		cat: 'Phone',
+		price: 10000,
+		favourite: false,
+	},
+];
+
+
+const cart = [];
